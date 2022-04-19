b713928ac0b52e547a77face55757554720c03e5
johnsjohns <o-ana@hotmail.com>
2022-04-19 17:19:19 -0300

    criando fica de personagem

---
+++ modelo/Personagem.java
@@ -0,0 +1,22 @@
+package modelo;
+public class Personagem {
+    
+    //Habilidades
+    private int str, dex, cons, inte, wis, cha;
+    //Pericias
+    private int abriFechadura,
+                acrobacia,
+                adestrarAnimais,
+                arteDaFuga,
+                avaliacao,
+                blefar,
+                cura,
+                decifrarEscrita,
+                diplomacia,
+                disfarce,
+                equilibrio,
+                escalar,
+
+
+
+}
